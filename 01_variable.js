07436bd2f10dad9d6017b6708c93856048cf7966
pathanshaista46 <pathanshaista46@gmail.com>
2023-09-20 17:47:10 +0530

    basicJS/assaignment

---
+++ 01_variable.js
@@ -0,0 +1,16 @@
+var firstName; // variable declaration
+firstName = "Shaista";  // Variable initialization
+console.log(firstName);
+firstName = "Miss.pathan"; // Variable update
+console.log(firstName);
+
+
+var lastName = "Pathan"; // variable declaration and initialization
+console.log(lastName);
+
+var height; // Variable Declaration
+console.log(height);
+height = "6"; // initialization
+console.log(height);
+height = "6.2 feet";// update
+console.log(height);
---
+++ variableAssign.js
@@ -0,0 +1,31 @@
+var myFullName; // variable declaration
+console.log(myFullName);
+myFullName = "Shaista Pathan"
+console.log(myFullName)
+
+myFullName="Shaista Parvez Pathan"
+console.log(myFullName);
+
+var myLove="I love only JS" 
+console.log(myLove);
+
+var firstName="Shaista"
+console.log(firstName);
+
+var lastName="Pathan"
+console.log(lastName);
+
+var age="24"
+console.log(age);
+
+var Address="Latur"
+console.log(Address);
+
+var pincode ="Pincode-413512"
+console.log(pincode);
+
+var Gender ="Gender-Female"
+console.log(Gender);
+
+var Married ="Maritial status-Single"
+console.log(Married);
