95c0bc735e0daca4dc271c2b629abc7c54732d34
pathanshaista46 <pathanshaista46@gmail.com>
2023-09-25 23:01:10 +0530

    String Assignment

---
+++ 03_stringBasics.js
@@ -0,0 +1,12 @@
+function stringBasics()
+{
+    var MyDreamCompany = "Amdocs";
+    console.log("My dream company is : ",MyDreamCompany);
+    var hobby1 = "Craft,";
+    var hobby2 = " sketching,";
+    var hobby3 = " Traveling";
+    console.log(hobby1+hobby2+hobby3);
+    console.log("length of hobby1 : ",hobby1.length,"length of hobby2 : ",hobby2.length,"length of hobby3 : ",hobby3.length);
+    console.log("addition of length : ",hobby1.length+hobby2.length+hobby3.length);
+}
+stringBasics()
---
+++ bankassaignment.js
@@ -0,0 +1,11 @@
+function bankDetails(arg1,arg2,arg3,arg4)
+{
+    console.log("Bank name:",arg1, "Account Number:",arg2, "city:",arg3,"pincode",arg4 )
+}
+
+
+bankDetails("CITI Bank",34567782345,"pune,431202")
+bankDetails("Axis Bank",785678235,"Mumbai",441202) 
+bankDetails("HDFC Bank",8976878687,"Pune",631204,"Open" )   
+
+    
